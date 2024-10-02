6e37a1b8836d66e406ff9aac6acfe1d237f2f23d
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-10-02 20:13:45 -0300

    fix(front): Let text input keep focus when pressing Send

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ const socket = io();
 const list = document.querySelector('#messageList');
 const listAnchor = document.querySelector('#messageList > .anchor');
 const textInput = document.querySelector('#textIn');
+const sendBtn = document.querySelector('#send');
 
 function addMsgToList(x) {
   const el = document.createElement('li');
@@ -11,11 +12,16 @@ function addMsgToList(x) {
   list.insertBefore(el, listAnchor);
 }
 
-document.querySelector('#send').addEventListener('click', (e) => {
+sendBtn.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+});
+
+sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   const text = textInput.value;
   console.log(`Sending "${text}"`);
+  textInput.value = '';
 
   // We send an event of type 'message' with `text` as the content.
   // The type ('message') is an arbitrary event name. It must match with
